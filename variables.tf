887593fad019889718d7e02c51b4f4a3ad3a77ee
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-08-29 11:09:35 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
+# variables.tf
+variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
+  default     = "mystical-rg"
 }
 
 variable "location" {
-  description = "Azure location for resources"
+  description = "Azure region where the resources will be deployed"
   type        = string
   default     = "Australia East"
+}
+
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  type        = string
+  default     = "mysticalstorageacct"
 }
