8304d65893d0bd43ae20c120bafed75818fbfacc
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-08-29 11:08:55 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-resource "azurerm_resource_group" "cosmicStorage_rg" {
-  name     = "cosmicStorageRG"
+# main.tf
+resource "azurerm_resource_group" "creative_rg" {
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "cosmicStorage" {
-  name                     = "cosmicstorageacct"
-  resource_group_name      = azurerm_resource_group.cosmicStorage_rg.name
-  location                 = var.location
+resource "azurerm_storage_account" "mysticalstorage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.creative_rg.name
+  location                 = azurerm_resource_group.creative_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  tags = {
-    environment = "development"
-    project     = "azure-storage-project"
-  }
 }
